4aaecde73587eaff50f8cfa8f94e241fbe9a6543
mpoapostolis <mpoapostolis@gmail.com>
2020-10-30 16:57:07 +0200

    keywords description SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,10 @@ export default function Home() {
         <title>omega trivia </title>
         <link rel="manifest" href="/manifest.json" />
 
-        <meta name="description" content="Free online trivia game" />
+        <meta
+          name="description"
+          content="Test your brain and get smarter with online trivial questions every day"
+        />
         <meta property="og:title" content="Omega Trivia" />
         <meta property="og:type" content="question.game" />
         <meta
@@ -52,7 +55,7 @@ export default function Home() {
         />
         <meta
           name="keywords"
-          content="trivia, free trivia, trivia game, trivia-game"
+          content="quiz game, trivia game, free quiz, trivia-game, quiz-game, free trivia,test your brain, brain exercise, get smarter"
         />
 
         <meta name="author" content="mpoapostolis@gmail.com" />
